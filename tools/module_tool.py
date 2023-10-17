e97be1978cff1342727889fbe9b5f0ed5473e127
Christopher751 <143539540+Christopher751@users.noreply.github.com>
2023-10-17 14:21:36 -0600

    Update module_tool.py
    data frame start

--- tools/module_tool.py
+++ tools/module_tool.py
@@ -1,3 +1,8 @@
-def yo():
-  print("yo!")
+import pandas as pd
+
+metadata_df = pd.DataFrame()
+metadata_df["nulls"] = df.isnull().sum()
+metadata_df["dtype_things"] = df.dtypes
+metadata_df["memory"] = df.memory_usage()
+metadata_df
         
